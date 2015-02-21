cff155eefa0724c7d355a45cfb10981af736d723
Marc Mauger <marc.mauger@igt.com>
2015-02-21 10:50:21 -0800

    added dynamic twitter card pages

--- server.js
+++ server.js
@@ -4,9 +4,27 @@ var express = require("express");
 var app = express();
 app.use(app.router);
 app.use(express.static(__dirname + "/public"));
+app.set('views', './views')
+app.set('view engine', 'jade');
 
-app.get("/audio", function(req, res){
-        res.send("Hello, World!");
+// Renders the Twitter Card player container page
+app.get('/cards/:author/:scid', function (req, res) {
+    // TODO: Get the soundcloud track ID
+    var trackId = 192289050;
+
+    res.render('card', {
+        author: req.params.author,
+        scId: req.params.scid,
+        scTrackId: trackId
+    });
 });
 
+// Renders the embedded soundcloud player iframe
+app.get('/player/:trackId', function (req, res) {
+    res.render('player', {
+        scTrackId: req.params.trackId
+    });
+});
+
+console.log('Listening on port ' + PORT);
 app.listen(PORT);
