9eacfd68c7442e55d41f2e2f86e5a784fe76ca51
Marc Mauger <simianarmy@gmail.com>
2016-05-29 11:33:39 -0700

    added v2 to play audio directly from s3.  Trying to cutout Soundcloud

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var PORT = process.env.PORT || 8080;
 var express = require("express");
+var qs = require('querystring');
 
 var app = express();
 app.use(app.router);
 app.use(express.static(__dirname + "/public"));
 app.set('views', './views')
 app.set('view engine', 'jade');
+
+S3_URL = 'https://s3-us-west-2.amazonaws.com/horsejs/';
 
 // Renders the Twitter Card player container page
 app.get('/cards/:author/:scid/:trackId', function (req, res) {
@@ -28,5 +31,29 @@ app.get('/player/:trackId', function (req, res) {
     });
 });
 
+// V2 - Attempting to serve audio files from S3 instead of Shitcloud
+// Renders the Twitter Card player container page
+app.get('/v2/cards/:author/:trackId', function (req, res) {
+    var max = 49;
+    var trackUrl = S3_URL + req.params.trackId + '.mp3';
+
+    res.render('v2/card', {
+        author: req.params.author,
+        trackId: req.params.trackId,
+        trackUrl: trackUrl,
+        trackUrlEncoded: qs.escape(trackUrl),
+        randomImage: (Math.floor(Math.random() * (max - 1)) + 1) + '.jpg'
+    });
+});
+
+// Renders the embedded soundcloud player iframe
+app.get('/v2/player/:trackId', function (req, res) {
+    var trackUrl = S3_URL + req.params.trackId + '.mp3';
+
+    res.render('v2/player', {
+        trackUrl: trackUrl,
+        trackUrlEncoded: qs.escape(trackUrl),
+    });
+});
 console.log('Listening on port ' + PORT);
 app.listen(PORT);
