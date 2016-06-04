0aa9def51cb164f5c194da5f31ae78b965cc5fb9
Marc Mauger <simianarmy@gmail.com>
2016-06-04 09:18:44 -0700

    added GA tracking.  Improved UI and disabled autoplay for iOS

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ app.set('views', './views')
 app.set('view engine', 'jade');
 
 S3_URL = 'https://s3-us-west-2.amazonaws.com/horsejs/';
+MAX_IMAGES = 49;
+
+function randomImageUrl () {
+    return 'https://twhispr.com/images/' + (Math.floor(Math.random() * (MAX_IMAGES - 1)) + 1) + '.jpg';
+}
 
 // Renders the Twitter Card player container page
 app.get('/cards/:author/:scid/:trackId', function (req, res) {
     // TODO: Get the soundcloud track ID
     //var trackId = 192289050;
-    var max = 49;
 
     res.render('card', {
         author: req.params.author,
         scId: req.params.scid,
         scTrackId: req.params.trackId,
-        randomImage: (Math.floor(Math.random() * (max - 1)) + 1) + '.jpg'
+        randomImage: randomImageUrl()
     });
 });
 
@@ -34,7 +38,6 @@ app.get('/player/:trackId', function (req, res) {
 // V2 - Attempting to serve audio files from S3 instead of Shitcloud
 // Renders the Twitter Card player container page
 app.get('/v2/cards/:author/:trackId', function (req, res) {
-    var max = 49;
     var trackUrl = S3_URL + req.params.trackId + '.mp3';
 
     res.render('v2/card', {
@@ -42,17 +45,22 @@ app.get('/v2/cards/:author/:trackId', function (req, res) {
         trackId: req.params.trackId,
         trackUrl: trackUrl,
         trackUrlEncoded: qs.escape(trackUrl),
-        randomImage: (Math.floor(Math.random() * (max - 1)) + 1) + '.jpg'
+        randomImage: randomImageUrl()
     });
 });
 
 // Renders the embedded soundcloud player iframe
 app.get('/v2/player/:trackId', function (req, res) {
     var trackUrl = S3_URL + req.params.trackId + '.mp3';
+    var ua = req.headers['user-agent'];
+    var iOS = /iPad|iPhone|iPod/.test(ua);
 
+    //console.log(req.useragent);
     res.render('v2/player', {
         trackUrl: trackUrl,
         trackUrlEncoded: qs.escape(trackUrl),
+        autoplay: !iOS,
+        randomImage: randomImageUrl()
     });
 });
 
